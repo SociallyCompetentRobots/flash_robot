9d033aee965503ccb911110fc4a625b1cfc20224
BrutusTT <brutusthetschiepel@gmail.com>
2018-12-04 17:12:25 +0000

    created constants for easier modification removed print in favour of logging added documentation set RATE to 20Hz since connection is slower anyways

--- flash_controller/nodes/laser_node.py
+++ flash_controller/nodes/laser_node.py
@@ -6,21 +6,26 @@ import sensor_msgs.msg
 
 from flash_controller.laser import Laser
 
+# Constants for LaserScan message
+RATE            =  20
+NUM_READINGS    =  133
+FRAME_ID        = "flash_robot_laser_link"
+
 
 def scan2ROS(val):
-    val                     = numpy.array(val, dtype = numpy.float32)
-    val                     = val / 1000.0
-    
-    laser_frequency         = 30
-    num_readings            = 133
+    """ Convert the laser scan readings into a ROS LaserScan message. 
 
+        FLASH sends the distance as mm hence the conversion into meters. The message will report
+        Be aware: ROS time instead of FLASH timestamp!
+    """
+    val                     = numpy.array(val, dtype = numpy.float32) / 1000.0
     scan                    = sensor_msgs.msg.LaserScan()
-    scan.header.stamp       = rospy.Time.now()                # we use the ROS time for the message!
-    scan.header.frame_id    = "flash_robot_laser_link"
+    scan.header.stamp       = rospy.Time.now()
+    scan.header.frame_id    = FRAME_ID
     scan.angle_min          = -1.2
     scan.angle_max          =  1.2
-    scan.angle_increment    = 3.14 / num_readings
-    scan.time_increment     = (1 / laser_frequency) / (num_readings)
+    scan.angle_increment    = 3.1415 / NUM_READINGS
+    scan.time_increment     = (1 / RATE) / (NUM_READINGS)
     scan.range_min          = 0.0
     scan.range_min          = 5.6
     scan.ranges             = val
@@ -32,14 +37,22 @@ def main():
 
     laser    = Laser()
     pub      = rospy.Publisher('/flash_robot/laser_scan', sensor_msgs.msg.LaserScan, queue_size = 1)
-    ros_rate = rospy.Rate(30)
+    ros_rate = rospy.Rate(RATE)
 
-    print("LaserNode started")
+    rospy.loginfo("LaserNode started")
+
+    # keep going till shutdown
     while not rospy.is_shutdown():
+        
+        # read the values from FLASH
         scan = laser.scan
+        
+        # scan might be None if we had a communication issue reading the laser scan from FLASH
         if scan:
             pub.publish(scan2ROS(scan[0]))
+
         ros_rate.sleep()
+
 
 if __name__ == '__main__':
     main()
