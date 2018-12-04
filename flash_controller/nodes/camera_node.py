86c5d812b4d182ebbfbf8280adfb62d2470cad5d
BrutusTT <brutusthetschiepel@gmail.com>
2018-12-04 17:34:20 +0000

    added camera node for /flash_robot/camera topic

---
+++ flash_controller/nodes/camera_node.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import cv2
+import rospy
+import cv_bridge
+import sensor_msgs.msg
+
+
+BRIDGE  = cv_bridge.CvBridge()
+RATE    = 10
+URI     = 'http://10.0.0.195:8081/video.mjpg'
+
+
+def main():
+    rospy.init_node('CameraNode')
+
+    cam      = cv2.VideoCapture(URI)
+    pub      = rospy.Publisher('/flash_robot/camera', sensor_msgs.msg.Image, queue_size = 0)
+    ros_rate = rospy.Rate(RATE)
+
+    rospy.loginfo("CameraNode started")
+
+    # keep going till shutdown
+    while cam.isOpened() and not rospy.is_shutdown():
+        ret, frame = cam.read()
+        if ret:
+            pub.publish(BRIDGE.cv2_to_imgmsg(frame, encoding = "rgb8"))
+        ros_rate.sleep()
+
+
+if __name__ == '__main__':
+    main()
