70de980ddece85fe8a9016940f933288a0eb7fb9
BrutusTT <brutusthetschiepel@gmail.com>
2018-12-17 12:26:12 +0000

    added documentation

--- flash_controller/src/flash_controller/joint.py
+++ flash_controller/src/flash_controller/joint.py
@@ -1,8 +1,20 @@
-import time
 
 
 class Joint(object):
+    """ The Joint class provides an API for individual joints.
     
+    The joint values can be read and modified in two different ways. 
+    
+    1)  The 'pos' values relate to a normalized position range sensible for the joint's function.
+        It is the preferred way to manipulate joints and it takes timing into account.
+
+    2)  The 'raw' values relate to the motors own angular position and will be specific to the 
+        individual motor.
+        Be aware: only use this way if you know what you are doing! Setting the raw values implies
+        that no trajectory generator is used and the motor will move as fast as possible to the 
+        targeted position. This might not be safe in all cases and might damage the motor.
+    """
+
     
     def __init__(self, urbi_wrapper, name, pos_min, pos_max, raw_zero):
         self.name       = name
@@ -14,39 +26,70 @@ class Joint(object):
         self.pos_max    = pos_max
     
     
-    def clipPositionLimits(self, position):
-        return max(min(self.pos_max, position), self.pos_min)
+    def clipPosLimits(self, pos):
+        """ Clips the given position to the min and max values which are given by the configuration.
+        
+        @param     pos:float - position
+        @return    clipped position
+        """
+        return max(min(self.pos_max, pos), self.pos_min)
 
 
-    def clipRawLimits(self, value):
-        return max(min(self.raw_max, value), self.raw_min)
+    def clipRawLimits(self, raw):
+        """ Clips the given position to the min and max values which are read from the motors.
+        
+        @param     raw:float - raw angle
+        @return    clipped position
+        """
+        return max(min(self.raw_max, raw), self.raw_min)
         
     
     @property
-    def val(self):        
+    def raw(self):
+        """ Returns the raw motor angle. """
         return float(self.uw.send(self.name + '.val')[0])
     
 
+    @raw.setter
+    def raw(self, raw):
+        """ Sets the raw motor angle to raw. 
+        
+        Be aware: this will set the value directly and moves the motor as fast as possible in this
+                  position. No trajectory generation is done!
+        """
+        self.uw.send('%s.val = %.4f' % (self.name, self.clipRawLimits(raw)))
+
+
     @property
-    def pos(self):        
-        """ """
+    def pos(self):
+        """ Returns the position angle. """
         return self.val - self.raw_zero
-    
+
+
     @pos.setter
     def pos(self, value):
-        self.uw.send('%s.MoveSpeed(%.4f, 30.0)' % (self.name, self.clipPositionLimits(value)))
-        time.sleep(0.5)
+        """ Sets the position angle by using the MoveSpeed method which will generate an
+            appropriate trajectory.
+        """
+        self.uw.send('%s.MoveSpeed(%.4f, 30.0)' % (self.name, self.clipPosLimits(value)))
 
 
     def center(self):
+        """ Moves the joint to it's center position which is based on the configuration. """
         if self.pos != 0.0:
             self.pos = 0.0
 
 
     def move(self, position, time):
-        position = self.clipLimits(position)
-        self.uw.send(self.name + '.Move(%.4f, %.4f)' % (position, time), time)
+        """ Moves the joint in position over time. """
+        self.uw.send(self.name + '.Move(%.4f, %.4f)' % (self.clipLimits(position), time), time)
 
 
     def __str__(self):
-        return "%s\t[%.3f, %.3f, %.3f]\t[%.3f, %.3f]\t%.3f" % (self.name, self.raw_min, self.raw_zero, self.raw_max, self.pos_min, self.pos_max, self.pos)
+        return "%s\t[%.3f, %.3f, %.3f]\t[%.3f, %.3f]\t%.3f" % ( self.name, 
+                                                                self.raw_min, 
+                                                                self.raw_zero, 
+                                                                self.raw_max, 
+                                                                self.pos_min, 
+                                                                self.pos_max, 
+                                                                self.pos )
