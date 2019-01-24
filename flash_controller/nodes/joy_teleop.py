d4cab2f601982a9ecf473349053e14e7d9cbd672
Jose Part <joselpart@users.noreply.github.com>
2019-01-24 15:18:40 +0000

    Fixed incompatibility with python3.

--- flash_controller/nodes/joy_teleop.py
+++ flash_controller/nodes/joy_teleop.py
@@ -261,7 +261,7 @@ class JoyTeleop:
         return self.service_types[service_name]
 
     def update_actions(self, evt=None):
-        for name, cmd in self.command_list.iteritems():
+        for name, cmd in self.command_list.items():
             if cmd['type'] != 'action':
                 continue
             if cmd['action_name'] in self.offline_actions:
