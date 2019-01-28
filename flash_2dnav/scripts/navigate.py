6f635b486087cc9fe9862b9a8cd26db5b4ce2b32
Jose Part <joselpart@users.noreply.github.com>
2019-01-28 18:30:11 +0000

    Changed server to execute behaviors.

--- flash_2dnav/scripts/navigate.py
+++ flash_2dnav/scripts/navigate.py
@@ -9,11 +9,8 @@ from move_base_msgs.msg import MoveBaseAction
 from move_base_msgs.msg import MoveBaseGoal
 from geometry_msgs.msg import Quaternion
 
-from flash_behaviors.msg import BehaveAction
-from flash_behaviors.msg import BehaveGoal
-
-from flash_behaviors.msg import SpeakAction
-from flash_behaviors.msg import SpeakGoal
+from flash_behaviors.msg import ActAction
+from flash_behaviors.msg import ActGoal
 
 from datetime import datetime
 
@@ -23,13 +20,10 @@ if __name__ == '__main__':
     rospy.init_node('move_base_client_node')
 
     move_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-    behave_client = actionlib.SimpleActionClient('behavior_server', BehaveAction)
-    speak_client = actionlib.SimpleActionClient('speech_server', SpeakAction)
+    act_client = actionlib.SimpleActionClient('action_server', ActAction)
 
-    rospy.loginfo("Waiting for servers...")
-    move_client.wait_for_server()
-    behave_client.wait_for_server()
-    speak_client.wait_for_server()
+    rospy.loginfo("Waiting for server...")
+    act_client.wait_for_server()
     rospy.loginfo("Server found!")
 
     # Node sleeps for 5 minutes.
@@ -60,26 +54,16 @@ if __name__ == '__main__':
     rospy.loginfo("Current time: %s", str(datetime.now()))
 
     # Perform behavior.
-    behave_goal = BehaveGoal()
+    act_goal = ActGoal()
 
-    behave_goal.behavior = 'Joy'
-    behave_goal.duration = 2
-    behave_goal.intensity = 8
+    act_goal.action = 'SpeakPointLeft("What is that?", 5, 4)'
 
-    speak_goal = SpeakGoal()
+    rospy.loginfo("Sending act goal...")
 
-    speak_goal.text = 'Hello. My name is Alyx!'
-    speak_goal.duration = 2
+    act_client.send_goal(act_goal)
+    act_client.wait_for_result()
 
-    rospy.loginfo("Sending behavior goal...")
-
-    behave_client.send_goal(behave_goal)
-    behave_client.wait_for_result()
-
-    rospy.loginfo("Sending speech goal...")
-
-    speak_client.send_goal(speak_goal)
-    speak_client.wait_for_result()
+    
 
     # Define goal -> go home
     # move_goal = MoveBaseGoal()
