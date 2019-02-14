1b79467166894cfbe33a0c4ab7e91613a59858f9
Jose Part <joselpart@users.noreply.github.com>
2019-02-14 16:52:56 +0000

    Remove sleep rate
    The sleep rate significantly slows down the video feed, probably
    due to the image buffer that VideoCapture works with.

--- flash_controller/nodes/camera_node.py
+++ flash_controller/nodes/camera_node.py
@@ -7,25 +7,22 @@ import sensor_msgs.msg
 
 
 BRIDGE  = cv_bridge.CvBridge()
-RATE    = 10
 URI     = 'http://10.0.0.195:8081/video.mjpg'
 
 
 def main():
     rospy.init_node('CameraNode')
 
-    cam      = cv2.VideoCapture(URI)
-    pub      = rospy.Publisher('/flash_robot/camera', sensor_msgs.msg.Image, queue_size = 1)
-    ros_rate = rospy.Rate(RATE)
+    cam = cv2.VideoCapture(URI)
+    pub = rospy.Publisher('/flash_robot/camera', sensor_msgs.msg.Image, queue_size = 1)
 
     rospy.loginfo("CameraNode started")
-
+    
     # keep going till shutdown
     while cam.isOpened() and not rospy.is_shutdown():
         ret, frame = cam.read()
         if ret:
             pub.publish(BRIDGE.cv2_to_imgmsg(frame, encoding = "bgr8"))
-        ros_rate.sleep()
 
 
 if __name__ == '__main__':
